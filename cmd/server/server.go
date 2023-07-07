aaaaa084d7b9db094b93e97b465928171803b454
Ekin Bulut <ekinbulut@gmail.com>
2023-07-07 10:31:26 +0300

    logger, ratelimitter & cors added

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -6,8 +6,12 @@ import (
 	"hermes/internals/api/routes"
 	"hermes/internals/services"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 type Server struct {
@@ -41,6 +45,8 @@ func (s *Server) Run() {
 
 	publisher := services.NewPublisher(&s.config.Rabbitmq)
 
+	s.configure()
+
 	// register routes
 	routes.MessageRouter(api, publisher)
 	routes.HealthCheck(api)
@@ -49,4 +55,27 @@ func (s *Server) Run() {
 	fmt.Printf("Server running on port %d\n", port)
 
 	log.Fatal(s.app.Listen(fmt.Sprintf(":%d", port)))
+}
+
+func (s *Server) configure() {
+	s.addCors()
+	s.addRatelimiter()
+	s.addLogger()
+}
+
+func (s *Server) addRatelimiter() {
+
+	s.app.Use(limiter.New(limiter.Config{
+		Max:               s.config.RateLimit.Max,
+		Expiration:        time.Duration(s.config.RateLimit.Expiration) * time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	}))
+}
+
+func (s *Server) addLogger() {
+	s.app.Use(logger.New())
+}
+
+func (s *Server) addCors() {
+	s.app.Use(cors.New())
 }
